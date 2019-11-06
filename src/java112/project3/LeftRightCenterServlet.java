56982baf4e6ed5d6d85fa100a3577bdab645d768
Elspeth Stalter-Clouse <estalterclouse@madisoncollge.edu>
2019-11-06 13:47:09 -0600

    Adding LeftRightCenter servlet.

---
+++ src/java112/project3/LeftRightCenterServlet.java
@@ -0,0 +1,27 @@
+package java112.project3;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+/**
+ * @author Michael, Dan, and Elspeth
+ */
+@WebServlet(
+    name = "leftRightCenter",
+    urlPatterns = { "/lrc" }
+)
+public class LeftRightCenter extends HttpServlet {
+
+    /**
+     * Handles HTTP GET Requests
+     *
+     * @param request HttpServletRequest object
+     * @param response HttpServletResponse object
+     * @exception ServletException if a Servlet failure occurs
+     * @exception IOException if an IO failure occurs
+     */
+    public void doGet(HttpServletRequest request, )
+}
