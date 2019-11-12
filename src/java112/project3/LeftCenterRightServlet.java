906427d7b2d6ad40a1f76f8dab635107d27a7506
Dan Robertson <dsrobertson@madisoncollege.edu>
2019-11-12 15:55:30 -0600

    stuff

--- src/java112/project3/LeftCenterRightServlet.java
+++ src/java112/project3/LeftCenterRightServlet.java
@@ -28,15 +28,33 @@ public class LeftCenterRightServlet extends HttpServlet {
      * @exception ServletException if a Servlet failure occurs
      * @exception IOException if an IO failure occurs
      */
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
 
             request.setAttribute("engine", engine);
-            engine.run();
 
             String url = "/mainPage.jsp";
 
             RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
             dispatcher.forward(request, response);
     }
+    /**
+     * Handles HTTP GET Requests
+     *
+     * @param request HttpServletRequest object
+     * @param response HttpServletResponse object
+     * @exception ServletException if a Servlet failure occurs
+     * @exception IOException if an IO failure occurs
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+            engine.run();
+            request.setAttribute("engine", engine);
+
+            String url = "/mainPage.jsp";
+
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+            dispatcher.forward(request, response);
+        }
+
 }
