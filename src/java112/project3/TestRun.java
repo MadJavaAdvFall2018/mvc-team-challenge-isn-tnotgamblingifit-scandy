c496eab37fdb506670326a0bb7af4c40ae172177
Elspeth Stalter-Clouse <estalterclouse@madisoncollge.edu>
2019-11-12 17:14:06 -0600

    Added an erroneous test class

---
+++ src/java112/project3/TestRun.java
@@ -0,0 +1,11 @@
+package java112.project3;
+
+public class TestRun {
+
+    public static void main(String[] args) {
+        DiceEngine sillyGoose = new DiceEngine();
+
+        sillyGoose.run();
+        System.out.println(sillyGoose.getPlayerOne());
+    }
+}
